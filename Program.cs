b1b0f92c5fb8168f755cf580e41617a9a31d30ed
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2021-04-10 03:30:39 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fecha;
+            DateTime salidaFecha;
+            bool flag=false ;
+            do
+            {
+                Console.WriteLine("Ingrese una fecha");
+                fecha = Console.ReadLine();
+                if (!DateTime.TryParse(fecha, out salidaFecha))
+                {
+                    Console.WriteLine("Usted no ha ingresado una fecha");
+                }
+                else
+                {
+                    flag = true;
+                    Console.WriteLine($"La fecha ingresada, se muestra la hora que es {salidaFecha.ToString("hh:mm:ss")}");
+                    Console.WriteLine("Presione una tecla para salir");
+                }
+            } while (flag==false);
+            Console.ReadKey();
+        }   
+    }
+}
